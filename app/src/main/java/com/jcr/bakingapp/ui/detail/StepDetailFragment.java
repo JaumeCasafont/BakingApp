0c5b7360335b11a8d887d8fd540231435e4368cf
JaumeCasafont <jaucr94@gmail.com>
2018-04-16 00:04:17 +0200

    added ExoPlayer

--- app/src/main/java/com/jcr/bakingapp/ui/detail/StepDetailFragment.java
+++ app/src/main/java/com/jcr/bakingapp/ui/detail/StepDetailFragment.java
@@ -1,16 +1,38 @@
 package com.jcr.bakingapp.ui.detail;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Config;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.TransferListener;
+import com.google.android.exoplayer2.util.Util;
 import com.jcr.bakingapp.Injection;
 import com.jcr.bakingapp.R;
 import com.jcr.bakingapp.data.models.Step;
@@ -24,11 +46,35 @@ import static com.jcr.bakingapp.ui.recipes.RecipesListActivity.EXTRA_RECIPE_ID;
 
 public class StepDetailFragment extends Fragment {
 
+    private static final String STATE_PLAYER_POSITION = "player_position";
+    private Context mContext;
     private StepDetailViewModel mViewModel;
     private int mStepId = 0;
     private FragmentStepDetailBinding mBinding;
+    private SimpleExoPlayer mExoPlayer;
+    private DataSource.Factory mediaDataSourceFactory;
+    private BandwidthMeter bandwidthMeter;
+    private DefaultTrackSelector trackSelector;
+    private boolean shouldAutoPlay;
 
     private final CompositeDisposable mDisposable = new CompositeDisposable();
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        shouldAutoPlay = true;
+        bandwidthMeter = new DefaultBandwidthMeter();
+        mediaDataSourceFactory = new DefaultDataSourceFactory(mContext,
+                Util.getUserAgent(mContext, "BakingApp"),
+                (TransferListener<? super DataSource>) bandwidthMeter);
+    }
 
     @Nullable
     @Override
@@ -36,6 +82,7 @@ public class StepDetailFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         mBinding = DataBindingUtil
                 .inflate(inflater, R.layout.fragment_step_detail, container, false);
+
         return mBinding.getRoot();
     }
 
@@ -65,6 +112,7 @@ public class StepDetailFragment extends Fragment {
     public void onStop() {
         super.onStop();
 
+        releasePlayer();
         mDisposable.clear();
     }
 
@@ -73,11 +121,44 @@ public class StepDetailFragment extends Fragment {
             ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(String.format(
                     getString(R.string.steps_detail_toolbar), String.valueOf(step.getId())));
         }
-        mBinding.stepId.setText(step.getShortDescription());
+        String videoUrl = step.getVideoURL();
+        if (videoUrl == null || videoUrl.isEmpty()) {
+            mBinding.playerView.setVisibility(View.GONE);
+        } else {
+            initializePlayer(videoUrl);
+        }
     }
 
     public void setStep(int stepId) {
         mStepId = stepId;
     }
 
+    private void initializePlayer(String videoUrl) {
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+
+        trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        mExoPlayer = ExoPlayerFactory.newSimpleInstance(mContext, trackSelector);
+
+        mBinding.playerView.setPlayer(mExoPlayer);
+
+        mExoPlayer.setPlayWhenReady(shouldAutoPlay);
+
+        DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+        MediaSource mediaSource = new ExtractorMediaSource(Uri.parse(videoUrl),
+                mediaDataSourceFactory, extractorsFactory, null, null);
+
+        mExoPlayer.prepare(mediaSource);
+    }
+
+    private void releasePlayer() {
+        if (mExoPlayer != null) {
+            shouldAutoPlay = mExoPlayer.getPlayWhenReady();
+            mExoPlayer.release();
+            mExoPlayer = null;
+            trackSelector = null;
+        }
+    }
 }
